5d314eae7b98da6f3d54fadb81aec7f7bf6c0e1e
hmtanrikulu <hmerttanrikulu@hotmail.com>
2024-03-06 15:28:09 +0300

    rotations

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+
+template <typename T>
+class Node {
+	T data;
+	Node<T>* right;
+	Node<T>* left;
+};
+
+template <typename T>
+class Tree {
+	Node<T>* Root;
+
+};
+
+
+
+int main() {
+
+	return 0;
+}
