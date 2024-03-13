adc56f67009163cdf9c1347fbd6ac92bf0148524
hmtanrikulu <hmerttanrikulu@hotmail.com>
2024-03-13 18:13:15 +0300

    on-going (implemented different versions for sake of better understanding)

--- solution.cpp
+++ solution.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -37,12 +37,6 @@ int getBalance(Node* node) {
 	return getHeight(node->left) - getHeight(node->right);
 }
 
-
-
-struct AVL {
-	Node* root;
-	int size;
-
 	Node* subtreeFirst(Node* node) {
 		Node* tmp = node;
 			do tmp = tmp->left;
@@ -67,18 +61,20 @@ struct AVL {
 		return parent;
 	}
 
-	void rightRotate(Node* up) {
+	Node* rightRotate(Node* up) {
 		Node* down = up->left;
-		Node* rightChildOfDown = down->right;
+		Node* rightChildOfDown = down->right; // read access violation ?
 
 		down->right = up;
 		up->left = rightChildOfDown;
 
-		up->height = max(getHeight(down->left), getHeight(up->right));
-		down->height = max(getHeight(down->left), getHeight(up->right));
+		up->height = max(getHeight(up->left), getHeight(up->right));
+		down->height = max(getHeight(down->left), getHeight(down->right));
+
+		return down;
 	}
 
-	void leftRotate(Node* up) {
+	Node* leftRotate(Node* up) {
 		Node* down = up->right;
 		Node* childOfDown = down->left;
 
@@ -88,6 +84,8 @@ struct AVL {
 
 		up->height = max(getHeight(down->left), getHeight(up->right));
 		down->height = max(getHeight(down->left), getHeight(up->right));
+
+		return down;
 	}
 
 	void inorder(Node* root)
@@ -99,68 +97,201 @@ struct AVL {
 		}
 	}
 
-	void insert(Node* root, int data) {
-		Node* newElement = new Node(data);
-		if (!root) {
-			root = newElement;
-			return;
-		}
-		Node* tmp = root;
-		Node* prev = nullptr;
 
-		while (tmp) { // tmp != nullptr ( sawp with this just in case!)
-			if (tmp->data > data) {
-				prev = tmp;
-				tmp = tmp->left;
-			}
-			else if (tmp->data < data) {
-				prev = tmp;
-				tmp = tmp->right;
-			}
+
+
+
+	/* Argument node = root 
+		
+	*/
+
+
+	Node* insert(Node* node, int data) {
+
+		/* base case*/
+		if (node == nullptr) return new Node(data);
+
+		if (data < node->data) {
+			node->left = insert(node->left, data);
 		}
-		if (prev->data < data) {
-			prev->right = newElement;
+		else if (data > node->data) {
+			node->right = insert(node->right, data);
 		}
-		else
-			prev->left = newElement;
+		else {
+			return node;
+		}
+
+		/* 2. Update the height*/
+		node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+
+		/* 3. Balance factor to check imbalance*/
+		int balance = getBalance(node);
+
+
+		/* Return new root everytime you make a change so that 
+			other ->left ->right pointer can update themselves;*/
+
+		// LL
+		if (balance > 1 && data < node->left->data) {
+			return rightRotate(node);
+		}
+		// RR
+		if (balance < -1 && data > node->right->data) {
+			return leftRotate(node);
+		}
+		//LR
+		if (balance > 1 && data > node->left->data) {
+			node->left = leftRotate(node->left);
+			return rightRotate(node);
+		}
+		// RL
+		if (balance < -1 && data < node->right->data) {
+			node->right = rightRotate(node->right);
+			return leftRotate(node);
+		}
+		return node;
 	}
-
-	
 
 /* We need this Node* cause it represents the subtree Root
 	that way we recursivly search below*/
 
-/* We return Node* in order ____*/
-	Node* deleteNode (Node* root, int key) {
-		// Base case
-		if (root == nullptr) return;
+	/*Delete the item in node <X> from <X>’s subtree
+		– If <X> is a leaf, detach from parent and return
+		– Otherwise, <X> has a child
+		∗ If <X> has a left child, swap items with the predecessor of <X> and recurse
+		∗ Otherwise <X> has a right child, swap items with the successor of <X> and recurse*/
 
-		if (root->data > key) {
-			root->left = deleteNode(root->left, key);
-			return root;
+	Node* deleteNode(Node* root, int key) {
+		// Base case
+		if (root == nullptr) return root;
+
+		if (key > root->data) {
+			root->right = deleteNode(root->right, key);
 		}
+		else if (key < root->data) {
+			root->left = deleteNode(root->left, key);
+		}
+
+		// Assume we found the key
+		else {
+			// Assume there are 1 or 0 child
+			if (root->left == nullptr) {
+				Node* tmp = root->right; // check for access violation error if necessary !
+				delete root;
+				return tmp;
+			}
+
+			else if (root->right == nullptr) {
+				Node* tmp = root->left;
+				delete root;
+				return tmp;
+			}
+
+			// Assume there are 2 branches
+			else {
+				Node* temp = successor(root);
+				root->data = temp->data;
+				deleteNode(root->right, temp->data);
+
+			}
+		}
+
+		/* 2. Update the height*/
+		root->height = 1 + max(root->left->height, root->right->height);
+
+		/* 3. Balance factor to check imbalance*/
+		int balance = getBalance(root);
+
+		/* 4. Now handle the rotations if necessary*/
+
+		// LL case
+		if (balance > 1 && key < root->left->data) {
+			return rightRotate(root);
+		}
+
+		// RR case
+		if (balance < -1 && key > root->right->data) {
+			return leftRotate(root);
+		}
+
+		// LR case
+		if (balance > 1 && key > root->left->data) {
+			root->left =leftRotate(root->left);
+			return rightRotate(root);
+		}
+
+		// RL case
+		if (balance < -1 && key < root->right->data) {
+			root->right = rightRotate(root->right);
+			return leftRotate(root);
+		}
+		return root;
+	}
+
+	Node* deleteNode(Node* node, int key) {
+
+		Node* tmp = node;
+		Node* prev = tmp;
+
+		while (tmp->data != key) {
+			if (key < tmp->data) {
+				prev = tmp;
+				tmp = tmp->left;
+			}
+			if (key > tmp->data) {
+				prev = tmp;
+				tmp = tmp->right;
+			}
+
+			// Assume you found the node
+			else if (tmp->data == key) {
+
+				// Assume it has 1 child or none
+
+				if (tmp->left == nullptr || tmp->right == nullptr) {
+					if (tmp->left) {
+
+						Node* tempChild = tmp->left;
+						tmp->data > prev->data ? prev->right = tempChild : prev->left = tempChild;
+						delete tmp;
+						break;
+					}
+					else {
+
+						Node* tempChild = tmp->right;
+						tmp->data > prev->data ? prev->right = tempChild : prev->left = tempChild;
+						delete tmp;
+						break;
+					}
+				}
+				// Assume it has 2 branches
+
+				if (tmp->right != nullptr && tmp->left != nullptr) {
+					// swap with successor
+
+					Node* tmp2 = successor(tmp);
+					tmp->data = tmp2->data;
+
+
+				}
+
+
+
+
+			}
+			
+			// Assume you couldn't find
+
+			else {
+
+			}
+
+		}
+
 
 
 	}
 
 
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-int main() {
-
-
-
-	return 0;
-}
+	
+	// & referans araştırmanı yap
